43c14a625d030fbe941f185bdc5773ea3639b49c
Niksooh <dymitri.thomas@gmail.com>
2024-06-06 11:49:09 +0200

    correctif et ajout index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './components/Home'
+import { Home } from './pages/Home'
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App/>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-    
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
