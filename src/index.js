841f59f07fbea0ceca4aac9ec98da9177504c97a
Niksooh <dymitri.thomas@gmail.com>
2024-06-06 16:36:22 +0200

    ajout multi de components

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home'
+import Home from './pages/Home';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
+    <Home/>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
